a41f25c7fdd334d780a6761c3b3c9a494198053e
Cosmitar <mhherrera31@gmail.com>
2021-03-19 13:36:54 -0300

    add typscript and themes

--- App.tsx
+++ App.tsx
@@ -1,21 +1,32 @@
 import React from "react";
+import { Appearance } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { Provider } from 'react-redux';
+import { Provider as PaperProvider, DarkTheme, DefaultTheme } from 'react-native-paper';
+// import * as eva from '@eva-design/eva';
+// import { ApplicationProvider } from '@ui-kitten/components';
 import configureStore from './src/store';
 import HomeScreen from "./src/screens/Home";
+import RegistrationScreen from "./src/screens/Register";
+import { RootStackParamList } from './types';
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator<RootStackParamList>();
+// const Stack = createStackNavigator();
 const store = configureStore();
+const theme = Appearance.getColorScheme() === 'dark' ? DarkTheme : DefaultTheme;
 
 export default function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <PaperProvider theme={theme}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Registration" component={RegistrationScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
     </Provider>
   );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import { StackNavigationProp } from '@react-navigation/stack';
 import { useSelector } from 'react-redux';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-paper';
+import { RootStackParamList } from '../../../types'
 
-function HomeScreen() {
+type HomeScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  'Home'
+>;
+
+type Props = {
+  navigation: HomeScreenNavigationProp;
+};
+
+const HomeScreen = ({ navigation }: Props) => {
   const flag = useSelector(state => state.flags.isLoading);
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>MindCo Relief Home Screen: {flag}</Text>
+      <Button icon="camera" mode="contained" onPress={() => navigation.push('Registration')}>
+        Press me
+      </Button>
     </View>
   );
 }
