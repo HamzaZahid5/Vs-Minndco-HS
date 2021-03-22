f25cbabd26b23608568b9571ac48958d5b3e1241
Cosmitar <mhherrera31@gmail.com>
2021-03-22 19:12:04 -0300

    quick theme inspector for debug

---
+++ src/utils/ThemeInspector.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useTheme } from 'react-native-paper';
+import { View, Text } from 'react-native';
+import { CustomThemeType, DefaultTheme } from './OriginalTheme';
+
+const getComponent = (id: string, val: string) => <View key={id}><Text style={{ color: val }}>{id}</Text></View>
+const inspect = (theme: CustomThemeType, pre = '') => {
+  const retVal:Array<any> = [];
+  Object.keys(theme).forEach(k => {
+    // @ts-ignore
+    const el = theme[k];
+    if (typeof el === 'object') {
+      inspect(el, k);
+    } else {
+      retVal.push(getComponent(pre + '_' + k, el));
+    }
+  })
+  return retVal;
+}
+export default () => {
+  const theme = DefaultTheme;
+  console.log(theme);
+  // @ts-ignore
+  const config = inspect(theme.colors);
+
+  return (<View style={{ backgroundColor: 'gray'}}>{config}</View>);
+}
