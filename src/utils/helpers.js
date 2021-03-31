e93778155f45526f8f9513fc7ebeb3b78994bca2
Cosmitar <mhherrera31@gmail.com>
2021-03-31 12:40:02 -0300

    helpers for activity screen

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,1 +1,25 @@
+import { TIPS_VR, TIPS_VIDEO, TIPS_AUDIO } from './constants'
 export const isFunction = f => typeof f === 'function';
+
+export const getTipsByActivityType = type => {
+  return {
+    'vr-met': translate('screens.activity.tipsVR'),
+    '2d-video': translate('screens.activity.tipsVideo'),
+    audio: translate('screens.activity.tipsAudio'),
+  }[type];
+};
+
+export const getIconByActivityType = type => {
+  switch (type) {
+    case 'vr-met':
+      return 'google-cardboard';
+    case 'reflection':
+      return 'pencil';
+    case '2d-video':
+      return 'video';
+    case 'audio':
+      return 'headphones';
+    default:
+      return 'star';
+  }
+};
