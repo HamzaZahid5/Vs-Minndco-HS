ff5f88a493d69005df385163bbd9d5f58c5a8e90
Cosmitar <mhherrera31@gmail.com>
2021-04-12 16:34:47 -0300

    add type

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux';
 import flagger, { FlagsState } from './slices/flags';
 // @ts-ignore
-import user from './slices/user';
+import user, { UserState } from './slices/user';
 
 export type RootState = {
   flags: FlagsState;
-  user: any;
+  user: UserState;
 }
 
 const reducer = combineReducers({
