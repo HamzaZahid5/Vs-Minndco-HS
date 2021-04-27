d5f11779ab45cbc335b287d2f1abfbab867d15d1
Cosmitar <mhherrera31@gmail.com>
2021-04-27 20:33:22 -0300

    mock up for 2 pages stats

--- src/screens/Statistics/index.js
+++ src/screens/Statistics/index.js
@@ -1,42 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { useTheme } from 'react-native-paper';
-import GenericPageLayout from '../../components/GenericPageLayout';
-import ScreenDecorator from '../../components/ScreenDecorator';
-import Carousel from '../../components/Carousel';
+import { Paragraph, useTheme } from 'react-native-paper';
+import { StackActions } from '@react-navigation/native';
 
-export default () => {
+import BigButton from '../../components/BigButton';
+import ScreenDecorator from '../../components/ScreenDecorator';
+import Carousel, { nextSlide } from '../../components/Carousel';
+import TodayActivityStatus from './TodayActivityStatus';
+import DailyActivityStreak from './DailyActivityStreak';
+import DailyActivityChart from './DailyActivityChart'
+import StressLevelsChart from './StressLevelsChart'
+import AverageStressLevel from './AverageStressLevel'
+import MostFrequentTriggers from './MostFrequentTriggers'
+
+export default ({ navigation }) => {
   const theme = useTheme();
   const styles = getStyles(theme);
+  const cRef = useRef()
+  // useEffect(() => {
+  //   if(cRef.current) {
+  //     setTimeout(() => {
+  //       console.log(cRef.current.currentIndex)
+  //       nextSlide(cRef);
+  //     }, 1000)
+  //     setTimeout(() => {
+  //       nextSlide(cRef);
+  //     }, 2000)
+      
+  //   }
+  // }, [cRef])
+  
   return (
     <ScreenDecorator>
       <Carousel
+        ref={cRef}
         items={[{
-          title: 'Constancy',
+          // title: 'Constancy',
           content: (
-            <View>
-              <Text style={{color: 'red'}}>Todays activity: done</Text>
-              <Text style={{color: 'red'}}>Current streak: 5 days</Text>
-              <Text style={{color: 'red'}}>Longest streak: 15 days</Text>
+            <View style={{ flex: 1, height: '100%' }}>
+              <View style={{ flex: 1, flexDirection: 'row', backgroundColor: '#f00a' }}>
+                <View style={{ flex: 1, backgroundColor: '#00fa', padding: 10 }}>
+                  <TodayActivityStatus done={false} />
+                </View>
+                <View style={{ flex: 1, backgroundColor: '#000a', padding: 10 }}>
+                  <DailyActivityStreak days={5} />
+                </View>
+              </View>
+              <View style={{ flex: 1, backgroundColor: '#0f0a', padding: 10 }}>
+                <DailyActivityChart />
+              </View>
+              <View style={{ position: 'absolute', bottom: -40, width: '100%', alignItems: 'center' }}>
+                <BigButton onPress={() => nextSlide(cRef)}>next</BigButton>
+              </View>
             </View>
           ),
         }, {
-          title: 'Triggers',
+          // title: 'Triggers',
           content: (
-            <View>
-              <Text style={{color: 'red'}}>Most frequent trigger: work</Text>
-              <Text style={{color: 'red'}}>Average level: 8</Text>
-              <Text style={{color: 'red'}}>last 10 records: __/---\___/-\_</Text>
+            <View style={{ flex: 1, height: '100%' }}>
+              <View style={{ flex: 1, flexDirection: 'row', backgroundColor: '#f00a' }}>
+                <View style={{ flex: 1, backgroundColor: '#00fa', padding: 10 }}>
+                  <MostFrequentTriggers triggers={['alcohol', 'work', 'other']} />
+                </View>
+                <View style={{ flex: 1, backgroundColor: '#000a', padding: 10 }}>
+                  <AverageStressLevel level={5} />
+                </View>
+              </View>
+              <View style={{ flex: 1, backgroundColor: '#0f0a', padding: 10 }}>
+                <StressLevelsChart />
+              </View>
+              <View style={{ position: 'absolute', bottom: -40, width: '100%', alignItems: 'center' }}>
+                <BigButton onPress={() => navigation.dispatch(StackActions.replace('PathEnding'))}>finish</BigButton>
+              </View>
             </View>
-          ),
-        }, {
-          title: 'Progress',
-          content: (
-            <View>
-              <Text style={{color: 'red'}}>Daily Activities: 3/48</Text>
-              <Text style={{color: 'red'}}>Progress: 6.25%</Text>
-              <Text style={{color: 'red'}}>Use of LS in last 10 days: 14x</Text>
-            </View>
+            // <View style={{ flex: 1, height: '100%' }}>
+            //   <Text style={{color: 'red'}}>Most frequent trigger: work</Text>
+            //   <Text style={{color: 'red'}}>Average level: 8</Text>
+            //   <Text style={{color: 'red'}}>last 10 records: __/---\___/-\_</Text>
+            //   <View style={{ position: 'absolute', bottom: -40, width: '100%', alignItems: 'center' }}>
+                
+            //   </View>
+            // </View>
           ),
         }]}
       />
