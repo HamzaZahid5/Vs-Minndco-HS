1e488792f646cdf2b55e33cf300ede07b5be56f5
Cosmitar <mhherrera31@gmail.com>
2021-05-20 11:19:57 -0300

    add selectoro to get module and level

--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -2,3 +2,6 @@ import { RootState } from './reducer';
 
 export const PROGRESS = (store:RootState) => store?.user?.data?.progress;
 export const KIT_ACTIVATED = (store:RootState) => store?.user?.data?.kit_id !== '';
+export const TREATMENT_MODULE_AND_LEVEL = (store:RootState) => (
+  [store?.user?.data.treatment_module, store?.user?.data.treatment_module]
+);
