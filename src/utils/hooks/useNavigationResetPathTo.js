b84c00bc7d503bacbe3f26ce9be50e6f9ced0c2f
Cosmitar <mhherrera31@gmail.com>
2021-05-20 16:27:57 -0300

    reset navigation path as hook

---
+++ src/utils/hooks/useNavigationResetPathTo.js
@@ -0,0 +1,16 @@
+// very lab option to avoid writing anoying code to reset navigation route
+// import { useEffect, useState } from 'react';
+
+export default navigation => {
+  const resetPathTo = (routeName, routeParams = {}) => navigation.reset({
+      index: 1,
+      routes: [
+        { name: 'Main' },
+        {
+          name: routeName,
+          params: routeParams ,
+        },
+      ],
+  });
+  return resetPathTo;
+}
