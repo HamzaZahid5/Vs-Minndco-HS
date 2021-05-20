f26800f42bdde0d9f8202dd3311bfc96d9d8a7d1
Cosmitar <mhherrera31@gmail.com>
2021-05-20 16:25:50 -0300

    refactor to return activity key

--- src/utils/hooks/useNextActivity.js
+++ src/utils/hooks/useNextActivity.js
@@ -1,20 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import useProgram from './useProgram';
 import { useSelector } from 'react-redux';
-import { findNextActivity } from '../helpers';
-import { PROGRESS, KIT_ACTIVATED } from '../../store/selectors';
+import { findNextActivity, buildActivityKey } from '../helpers';
+import { PROGRESS, KIT_ACTIVATED, TREATMENT_MODULE_AND_LEVEL } from '../../store/selectors';
 
 export default () => {
+  // STATE
   const [nextActivity, setNextActivity] = useState();
+  const [nextActivityKey, setNextActivityKey] = useState();
+  
+  // REDUX SELECTORS
   const progress = useSelector(PROGRESS);
   const includeVR = useSelector(KIT_ACTIVATED);
+  const [mId, lId] = useSelector(TREATMENT_MODULE_AND_LEVEL);
+  
+  // PROGRAM
   const program  = useProgram();
 
   useEffect(() => {
     if (program) {
-      setNextActivity(findNextActivity(program, [...progress].pop(), includeVR));
+      const nextActivity = findNextActivity(program, [...progress].pop(), includeVR);
+      setNextActivity(nextActivity);
+      setNextActivityKey(buildActivityKey(mId, lId, nextActivity.id));
     }
-  }, [program, progress, includeVR]);
+  }, [program, progress, includeVR, mId, lId]);
 
-  return nextActivity;
+  return [nextActivity, nextActivityKey];
 }
