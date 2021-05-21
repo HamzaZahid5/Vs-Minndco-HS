65bb388a836dd4f04bea515298fdb092b39f15cf
Cosmitar <mhherrera31@gmail.com>
2021-05-20 23:34:04 -0300

    connect stats to real data

--- src/screens/Statistics/CompletionChart.js
+++ src/screens/Statistics/CompletionChart.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, Dimensions } from 'react-native';
 import { Paragraph, useTheme } from 'react-native-paper';
 import {
   ProgressChart,
 } from "react-native-chart-kit";
+import useCompletion from '../../utils/hooks/useCompletion';
 
 const chartConfig = {
   backgroundGradientFrom: "transparent",
@@ -15,12 +16,18 @@ const chartConfig = {
   // barPercentage: 0.5,
   // useShadowColorFromDataset: false // optional
 };
-export default ({ progress = 0.85 }) => {
+export default () => {
   const theme = useTheme();
-  const data = {
-    labels: ["completion"], // optional
-    data: [progress]
-  };
+  const progress = useCompletion();
+  const [data, setData] = useState();
+  useEffect(() => {
+    if(progress) {
+      setData({
+        labels: ["completion"], // optional
+        data: [progress / 100]
+      })
+    }
+  }, [progress])
   const SIZE = Math.min(Dimensions.get('window').width, Dimensions.get('window').height);
   return (
     <View style={{
@@ -36,18 +43,20 @@ export default ({ progress = 0.85 }) => {
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', width: SIZE - SIZE / 8 }}>
         <View style={{ flex: 1 }}/>
         <View style={{ position: 'absolute' }}>
-          <ProgressChart
-            data={data}
-            width={SIZE}
-            height={SIZE}
-            strokeWidth={12}
-            radius={SIZE / 4}
-            hideLegend
-            chartConfig={chartConfig}
-          />
+          {data && (
+            <ProgressChart
+              data={data}
+              width={SIZE}
+              height={SIZE}
+              strokeWidth={12}
+              radius={SIZE / 4}
+              hideLegend
+              chartConfig={chartConfig}
+            />
+          )}
         </View>
         <View style={{ position: 'absolute', width: SIZE, height: SIZE, alignItems: 'center', justifyContent: 'center' }}>
-          <Paragraph style={{ fontSize: 40}}>{progress * 100}%</Paragraph>
+          <Paragraph style={{ fontSize: 40}}>{progress}%</Paragraph>
         </View>
       </View>
     </View>
--- src/screens/Statistics/TodayActivityStatus.js
+++ src/screens/Statistics/TodayActivityStatus.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Paragraph, useTheme } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { useSelector } from 'react-redux';
+import moment from 'moment';
+import { LAST_ACTIVITY_AT } from '../../store/selectors';
 
-export default ({ done }) => {
+export default () => {
   const theme = useTheme();
+  const lastActivityAt = useSelector(LAST_ACTIVITY_AT);
+  const todaysActivityDone = moment(lastActivityAt).format('YYY-MM-DD') !== moment().format('YYYY-MM-DD');
+  
   return (
     <View style={{
       backgroundColor: 'lime',
@@ -17,9 +23,9 @@ export default ({ done }) => {
         Daily activity
       </Paragraph>
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Icon name={ done ? "progress-check" : "progress-alert" } size={60} color="yellow" />
+        <Icon name={ todaysActivityDone ? "progress-check" : "progress-alert" } size={60} color="yellow" />
         <Paragraph style={{ ...theme.fonts.regular }}>
-          { done ? 'done!' : 'pending...' }
+          { todaysActivityDone ? 'done!' : 'pending...' }
         </Paragraph>
       </View>
     </View>
