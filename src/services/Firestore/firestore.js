bfb834504a796e56ef22c47ad92f84e1cef07183
Cosmitar <mhherrera31@gmail.com>
2021-05-20 23:33:38 -0300

    expose timestamp type

--- src/services/Firestore/firestore.js
+++ src/services/Firestore/firestore.js
@@ -1,4 +1,5 @@
 const defFunction = () => window.firebase.firestore();
 Object.defineProperty(defFunction, 'FieldValue', { get: () => window.firebase.firestore.FieldValue })
+Object.defineProperty(defFunction, 'Timestamp', { get: () => window.firebase.firestore.Timestamp })
 
 export default defFunction;
