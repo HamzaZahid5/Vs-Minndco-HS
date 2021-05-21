61a022ea30c1972aad0489788ba403ef49e62a4d
Cosmitar <mhherrera31@gmail.com>
2021-05-21 15:20:58 -0300

    fix rate stars for Andoird

--- src/screens/PathEnding/HeaderRating.js
+++ src/screens/PathEnding/HeaderRating.js
@@ -14,25 +14,25 @@ export default ({ onVote }) => {
           style={{}}
           onPress={() => setVote(1)}
         >
-          <Text style={[styles.star, vote < 1 ? styles.unselected : null]}>⭐️</Text>
+          <Text style={[styles.star, vote < 1 ? styles.unselected : styles.selected]}>⭐️</Text>
         </Pressable>
         <Pressable
           style={{}}
           onPress={() => setVote(2)}
         >
-          <Text style={[styles.star, vote < 2 ? styles.unselected : null]}>⭐️</Text>
+          <Text style={[styles.star, vote < 2 ? styles.unselected : styles.selected]}>⭐️</Text>
         </Pressable>
         <Pressable
           style={{}}
           onPress={() => setVote(3)}
         >
-          <Text style={[styles.star, vote < 3 ? styles.unselected : null]}>⭐️</Text>
+          <Text style={[styles.star, vote < 3 ? styles.unselected : styles.selected]}>⭐️</Text>
         </Pressable>
         <Pressable
           style={{}}
           onPress={() => setVote(4)}
         >
-          <Text style={[styles.star, vote < 4 ? styles.unselected : null]}>⭐️</Text>
+          <Text style={[styles.star, vote < 4 ? styles.unselected : styles.selected]}>⭐️</Text>
         </Pressable>
       </View>
     </View>
@@ -52,8 +52,18 @@ const getStyles = theme => StyleSheet.create({
     fontSize: 30,
     margin: 5,
   },
+  selected: {
+    color: 'yellow',
+    // textShadow: '0 0 0 gray',
+    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+    textShadowOffset: {width: -1, height: 1},
+    textShadowRadius: 1,
+  },
   unselected: {
     color: 'transparent',
-    textShadow: '0 0 0 gray',
+    // textShadow: '0 0 0 gray',
+    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+    textShadowOffset: {width: -1, height: 1},
+    textShadowRadius: 1,
   }
 });
