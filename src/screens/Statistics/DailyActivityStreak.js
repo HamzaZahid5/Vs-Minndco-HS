f0276e15497be888d5c9d97277a2e51c61e58072
Cosmitar <mhherrera31@gmail.com>
2021-05-22 12:11:53 -0300

    adapt stats card for streak

--- src/screens/Statistics/DailyActivityStreak.js
+++ src/screens/Statistics/DailyActivityStreak.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Paragraph, useTheme } from 'react-native-paper';
+import { useSelector } from 'react-redux';
+import { ACTIVITY_DAYS_IN_A_ROW } from '../../store/selectors';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default ({ days }) => {
   const theme = useTheme();
+  const streakCount = useSelector(ACTIVITY_DAYS_IN_A_ROW);
   return (
     <View style={{
       backgroundColor: 'lime',
@@ -14,16 +17,25 @@ export default ({ days }) => {
       justifyContent: 'flex-start',
     }}>
       <Paragraph style={{ ...theme.fonts.heading2, fontSize: 20 }}>
-        Streak
+        Habit
       </Paragraph>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Paragraph style={{ ...theme.fonts.heading1, fontSize: 50, lineHeight: 50, }}>
-          { days }
-        </Paragraph>
-        <Paragraph style={{ ...theme.fonts.regular }}>
-          { days === 1 ? 'day' : 'days' }
-        </Paragraph>
-      </View>
+      {streakCount === 0 && (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Paragraph style={{ ...theme.fonts.regular }}>
+            Preform one activity a day to make it a habit
+          </Paragraph>
+        </View>
+      )}
+      {streakCount !== 0 && (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Paragraph style={{ ...theme.fonts.heading1, fontSize: 50, lineHeight: 50, }}>
+            { streakCount }
+          </Paragraph>
+          <Paragraph style={{ ...theme.fonts.regular }}>
+            { streakCount === 1 ? 'day' : 'days' }
+          </Paragraph>
+        </View>
+      )}
     </View>
   );
 };
