6ed4f386cd2e6dc4f96c779e477d77bc33ab818a
Cosmitar <mhherrera31@gmail.com>
2021-05-24 19:11:03 -0300

    adjust readability

--- src/components/ReadActivity/index.js
+++ src/components/ReadActivity/index.js
@@ -1,30 +1,59 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Paragraph, useTheme } from 'react-native-paper';
 import Carousel from '../Carousel';
 
 export default ({ content }) => {
-  console.log(content)
+  const theme = useTheme();
+  const styles = getStyles(theme);
   return (
     <Carousel
-      items={content.pages.map(page => ({
-        title: content.title,
+      items={content.pages.map((page, idx) => ({
+        // title: content.title,
         content: (
-          <View style={{ flex: 1, height: '100%' }}>
-            <Text>{page}</Text>
+          <>
+          <Text style={styles.title}>{content.title}</Text>
+          <View style={styles.container}>
+            <Text style={styles.number}>0{idx + 1}</Text>
+            <Paragraph style={styles.text}>{page}</Paragraph>
           </View>
+          </>
         )
       }))}
     />
   );
 };
 
-const styles = StyleSheet.create({
+const getStyles = theme => StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    height: '100%',
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 20,
     justifyContent: 'center',
-    padding: 10,
-    // borderWidth: 1,
-    borderColor: 'peru',
+    shadowColor: '#664AB9',
+    shadowOffset: { width: 1, height: 1 },
+    shadowOpacity: 0.4,
+    shadowRadius: 3,
+    elevation: 4,
   },
+  title: {
+    ...theme.fontsHelper.heading2,
+    marginBottom: 10,
+  },
+  text: {
+    ...theme.fonts.thin,
+    fontSize: 20,
+    lineHeight: 30
+  },
+  number: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    margin: 20,
+    fontSize: 40,
+    color: '#0005',
+    fontWeight: 'bold',
+  }
 });
