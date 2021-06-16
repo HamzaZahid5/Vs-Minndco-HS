734245dbb5f4760524fb227cdc4bc2392dc96a66
Cosmitar <mhherrera31@gmail.com>
2021-06-16 17:48:00 -0300

    fix colouring

--- src/screens/StressRate/AnimatedRateRow.js
+++ src/screens/StressRate/AnimatedRateRow.js
@@ -1,16 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import { View, Text, StyleSheet } from 'react-native';
 import Color from 'color';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useTheme } from 'react-native-paper';
+import { Dimensions } from 'react-native';
+import { Platform } from 'react-native';
 
-export default ({
-  selected,
-  onSelected,
-  delay,
-  value = 0,
-  color = 'white',
-}) => {
+const AnimatedRateRow = ({ selected, onSelected, delay, value = 0 }) => {
   const theme = useTheme();
   const [highlight, setHighlight] = useState();
   useEffect(() => {
@@ -22,56 +19,67 @@ export default ({
     } else {
       setHighlight(false);
     }
-  }, [selected])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selected]);
   return (
-    <LinearGradient
-      colors={[
-        "#eef5f5",
-        '#e2e9f1',
-        '#e2e9f1',
-        '#e2e9f1',
-        '#cbdcea',
-      ]}
-      style={styles.rowOption}
-    >
+    <LinearGradient colors={['#eef5f5', '#e2e9f1', '#e2e9f1', '#e2e9f1', '#cbdcea']} style={styles.rowOption}>
       {/* <View style={styles.rowOption}> */}
-      <Text style={[styles.rowNumber, {
-        color: highlight ? Color('#3D9AD5').mix(Color('#d53d4c'), value/10)   : Color('white').darken(value/10 + 0.1).toString(),
-      }]}>{value}</Text>
-      <Text style={styles.rowNumberSmall}>{value < 10 ? `0${value}` : value}</Text>
-    {/* </View> */}
-      </LinearGradient>
+      <Text
+        style={[
+          styles.rowNumber,
+          {
+            color: highlight
+              ? Color('#3D9AD5')
+                  .mix(Color('#d53d4c'), value / 10)
+                  .toString()
+              : Color('white')
+                  .darken(value / 10 + 0.1)
+                  .toString(),
+          },
+        ]}
+      >
+        {value}
+      </Text>
+      {/* <Text style={styles.rowNumberSmall}>{value < 10 ? `0${value}` : value}</Text> */}
+      {/* </View> */}
+    </LinearGradient>
   );
 };
+
+AnimatedRateRow.propTypes = {
+  selected: PropTypes.bool,
+  onSelected: PropTypes.func,
+  delay: PropTypes.number,
+  value: PropTypes.number,
+};
+
+export default AnimatedRateRow;
 
 const styles = StyleSheet.create({
   rowOption: {
     flex: 1,
-    // borderBottomWidth: 3,
-    // borderColor: 'white',
-    // borderStyle: 'dashed',
     justifyContent: 'center',
-    alignItems: 'center',
     overflow: 'hidden',
-    flexWrap: 'wrap',
     borderRadius: 10,
     marginHorizontal: 5,
     marginVertical: 0.5,
-    // maxHeight: '10%',
   },
   rowNumber: {
-    position: 'absolute',
-    fontSize: 120,
-    fontStyle: 'italic',
+    fontFamily: 'Graphik-Regular',
+    // position: 'absolute',
+    fontSize: (Dimensions.get('window').height / 10) * 1.4,
+    lineHeight: (Dimensions.get('window').height / 10) * (Platform.OS === 'ios' ? 1.2 : 1.6),
     // fontWeight: 'bold',
     opacity: 0.75,
+    // backgroundColor: '#f00a',
+    textAlign: 'center',
   },
   rowNumberSmall: {
     fontSize: 30,
     position: 'absolute',
-    left: 0,
+    right: 0,
     bottom: 0,
     color: 'white',
     // fontWeight: 'bold',
-  }
-})
+  },
+});
