0d25f57a0b5f14aa28f30692c3fafe6269f62d44
Cosmitar <mhherrera31@gmail.com>
2021-06-17 17:33:10 -0300

    add bg image to header

--- src/screens/PathEnding/HeaderStatistics.js
+++ src/screens/PathEnding/HeaderStatistics.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Image, Dimensions, StyleSheet } from 'react-native';
 import { Headline, Paragraph, useTheme } from 'react-native-paper';
 import Color from 'color';
 
@@ -8,12 +8,15 @@ const HeaderStatistics = () => {
   const styles = getStyles(theme);
   return (
     <View style={styles.container}>
-      <Headline style={styles.headline}>Keep an eye on your statistics</Headline>
-      <View style={styles.main}>
-        <Paragraph style={styles.paragraph}>
-          They are a valuable source of personal infromation about your behavioral change.
-        </Paragraph>
+      <View style={{ position: 'absolute' }}>
+        <Headline style={styles.headline}>Keep an eye on your statistics</Headline>
+        <View style={styles.main}>
+          <Paragraph style={styles.paragraph}>
+            They are a valuable source of personal infromation about your behavioral change.
+          </Paragraph>
+        </View>
       </View>
+      <Image style={styles.topImage} source={require('../../../assets/images/header_bg_2.png')} resizeMode="cover" />
     </View>
   );
 };
@@ -32,11 +35,16 @@ const getStyles = theme =>
       height: '100%',
       justifyContent: 'flex-start',
       alignItems: 'center',
-      padding: 20,
+      paddingHorizontal: 20,
     },
     main: {
       flexDirection: 'row',
       alignItems: 'center',
       justifyContent: 'center',
+    },
+    topImage: {
+      // width: Dimensions.get('window').width,
+      // backgroundColor: '#f00a',
+      opacity: 0.75,
     },
   });
