7e7be6c56e1b56178d5f60dea70da02deaf67346
Joaco <joakoitria98@gmail.com>
2021-07-28 11:42:21 -0300

    Added localization function

---
+++ src/utils/localization.ts
@@ -0,0 +1,71 @@
+import * as Localization from 'expo-localization';
+
+import i18n from 'i18n-js';
+import { memoize } from 'lodash';
+import storage from '@react-native-firebase/storage';
+
+const getDictionaryFile = async (lang: string): Promise<Record<string, unknown>> => {
+  let url: string;
+  try {
+    url = await storage()
+      .ref()
+      // @TODO move file name to config
+      .child(`dictionaries/v4.16.3_${lang}.json`)
+      .getDownloadURL();
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+  }
+  return new Promise(resolve => {
+    fetch(url).then(response => {
+      resolve(response.json());
+    });
+  });
+};
+
+const translationGetters = {
+  // lazy requires (metro bundler does not support symlinks)
+  en: (): Promise<Record<string, unknown>> => getDictionaryFile('en'),
+  es: (): Promise<Record<string, unknown>> => getDictionaryFile('es'),
+};
+
+export const translate = memoize(
+  (key, config) => i18n.t(key, config),
+  (key, config) => (config ? key + JSON.stringify(config) : key),
+);
+
+export const getLocale = (): string => i18n.locale;
+export const getDayRefFormat = (locale: string = getLocale()) => {
+  switch (locale) {
+    case 'es':
+      return 'DD [de] MMMM';
+    default:
+      return 'MMMM Do';
+  }
+};
+
+// in Spanish verbs in past participle are pluralizable meanwhile in English they aren't.
+// currently only en is not pluralizeble.
+export const isPastParticiplePluralizable = (locale: string = getLocale()) =>
+  ({
+    en: false,
+    es: true,
+  }[locale]);
+
+export const setI18nConfig = async (callback: (value: boolean) => undefined) => {
+  // fallback if no available language fits
+  const fallback = 'en';
+
+  let { locale } = await Localization.getLocalizationAsync();
+  if (!Object.keys(translationGetters).includes(locale)) locale = fallback;
+
+  const languageTag: keyof typeof translationGetters = locale as keyof typeof translationGetters;
+
+  const dictionary = await translationGetters[languageTag]();
+  // set i18n-js config
+  // clear translation cache
+  if (translate.cache.clear) translate.cache.clear();
+  i18n.translations = { [languageTag]: dictionary };
+  i18n.locale = languageTag;
+  callback(true);
+};
