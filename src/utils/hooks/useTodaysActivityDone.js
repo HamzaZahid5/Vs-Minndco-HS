f07aaffd41712bc946cf9e7d0ed5aae0a7c101bd
Joaco <joakoitria98@gmail.com>
2021-08-04 15:43:39 -0300

    See if progress is not empty, when checking for today activity done

--- src/utils/hooks/useTodaysActivityDone.js
+++ src/utils/hooks/useTodaysActivityDone.js
@@ -1,10 +1,12 @@
 import moment from 'moment';
 import { useSelector } from 'react-redux';
-import { LAST_ACTIVITY_AT } from '../../store/selectors';
+import { LAST_ACTIVITY_AT, PROGRESS } from '../../store/selectors';
 
 export default () => {
   const lastActivityAt = useSelector(LAST_ACTIVITY_AT);
-  const todaysActivityDone = moment(lastActivityAt).format('YYYY-MM-DD') === moment().format('YYYY-MM-DD');
+  const progress = useSelector(PROGRESS); // If no activity was done, today's activity hasn't been done  --  This is needed for new users
+  const todaysActivityDone =
+    moment(lastActivityAt).format('YYYY-MM-DD') === moment().format('YYYY-MM-DD') && progress.length > 0;
 
   return todaysActivityDone;
 };
