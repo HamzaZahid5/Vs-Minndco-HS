37f7473a7933e19d31c074153c54c976eb939eda
Joaco <joakoitria98@gmail.com>
2021-08-06 11:03:25 -0300

    react-native-orientation-locker added, and react-native-orientation removed

--- android/app/src/main/java/com/mindcorelief/MainApplication.java
+++ android/app/src/main/java/com/mindcorelief/MainApplication.java
@@ -22,6 +22,7 @@ import expo.modules.constants.ConstantsPackage;
 import expo.modules.permissions.PermissionsPackage;
 import expo.modules.filesystem.FileSystemPackage;
 import expo.modules.updates.UpdatesController;
+import org.wonday.orientation.OrientationActivityLifecycle;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
@@ -79,6 +80,7 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+    registerActivityLifecycleCallbacks(OrientationActivityLifecycle.getInstance());
 
     if (!BuildConfig.DEBUG) {
       UpdatesController.initialize(this);
