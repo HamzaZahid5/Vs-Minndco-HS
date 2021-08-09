147d960df5985fcdafa730797aedc38c7712c9be
Cosmitar <mhherrera31@gmail.com>
2021-08-09 11:52:50 -0300

    include HAS_COACH_MESSAGES selector into USER_SUPPORT_PROFILE

--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -11,7 +11,6 @@ export const LAST_ACTIVITY_AT = (store: RootState) => {
   return fireBaseDate ? fireBaseDate.toDate() : undefined;
 };
 export const ACTIVITY_DAYS_IN_A_ROW = (store: RootState) => store?.user?.data?.statistics?.activity_days_in_a_row;
-export const HAS_COACH_MESSAGES = (store: RootState) => store?.user?.data?.flags?.has_coach_messages;
 export const USER_PROFILE = (store: RootState) => {
   const { language, gender } = store?.user?.data;
   return { language, gender };
