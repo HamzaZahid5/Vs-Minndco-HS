ddf0bb9a1283793abdcb9c88220442f35968f953
Joaco <joakoitria98@gmail.com>
2021-08-12 11:14:55 -0300

    StressRate migrated to ts

--- src/screens/StressRate/index.tsx
+++ src/screens/StressRate/index.tsx
@@ -1,42 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { TouchableOpacity } from 'react-native';
-import { useTheme } from 'react-native-paper';
 import { useDispatch } from 'react-redux';
 import Row from './AnimatedRateRow';
+// @ts-ignore: non-ts file
 import ScreenDecorator from '../../components/ScreenDecorator';
+import { DefaultScreenPropType } from '../../../types';
 
-const StressRate = ({ navigation }) => {
-  const theme = useTheme();
+const StressRate = ({ navigation }: DefaultScreenPropType<'StressRate'>) => {
   const dispatch = useDispatch();
-  const [selected, setSelection] = useState();
+  const [selected, setSelection] = useState<number>();
   const rate = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const colorsIni = [
-    '#2B69DA',
-    '#31CCCC',
-    '#37AD38',
-    '#68C33B',
-    '#9DDB3D',
-    '#FBF042',
-    '#F8BE54',
-    '#F8BE54',
-    '#F48568',
-    '#F34C78',
-  ];
-  const colorsEnd = [
-    '#014BD2',
-    '#01BFBF',
-    '#019800',
-    '#3CB102',
-    '#7FCE00',
-    '#BDE500',
-    '#CCBC00',
-    '#F4A414',
-    '#F1542B',
-    '#EC0040',
-  ];
 
-  const onSelected = idx => {
+  const onSelected = (idx: number) => {
     if (idx === selected) {
       dispatch({ type: 'currentStress/setStressLevel', payload: idx });
       navigation.push('StressTrigger');
@@ -56,7 +32,12 @@ const StressRate = ({ navigation }) => {
     <ScreenDecorator>
       {rate.reverse().map(i => (
         <TouchableOpacity key={`rate_${i}`} onPress={() => (!selected ? setSelection(i) : null)} style={{ flex: 1 }}>
-          <Row selected={selected >= i} delay={i * 50} value={i} onSelected={() => onSelected(i)} />
+          <Row
+            selected={selected !== undefined ? selected >= i : false}
+            delay={i * 50}
+            value={i}
+            onSelected={() => onSelected(i)}
+          />
         </TouchableOpacity>
       ))}
     </ScreenDecorator>
