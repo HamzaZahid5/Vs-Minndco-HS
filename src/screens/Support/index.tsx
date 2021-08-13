c3c40981b1af74c83fafec2cd0d957b8c48d1fe9
Cosmitar <mhherrera31@gmail.com>
2021-08-13 19:10:04 -0300

    escape translated text

--- src/screens/Support/index.tsx
+++ src/screens/Support/index.tsx
@@ -85,8 +85,9 @@ const Support = ({
       true;
   `;
   const runFirst = `
+    /* keep this email well formatted for Crisp verification but anonymous */
     window.injectedEmail = 'private@gmail.com';
-    window.startingText = ${translate('screens.Support.starting-chat')};
+    window.startingText = \`${translate('screens.Support.starting-chat')}\`;
   `;
 
   return (
@@ -162,7 +163,11 @@ const Support = ({
       />
       {!webViewVisible && (
         <View style={[styles.overlay, webViewVisible ? styles.overlayHidden : null]}>
-          <Text>{isCoachingSupport ? translate('screens.Support.starting-coach-chat') : translate('screens.Support.starting-support-chat')}</Text>
+          <Text>
+            {isCoachingSupport
+              ? translate('screens.Support.starting-coach-chat')
+              : translate('screens.Support.starting-support-chat')}
+          </Text>
         </View>
       )}
       <KeyboardSpacer />
