e700b6ac092468e879514da2d38756e2e6aa18cf
Joaco <joakoitria98@gmail.com>
2021-08-18 15:27:46 -0300

    Using useDeviceOrientationChange instead of useOrientationChange in useOrientation hook

--- src/utils/hooks/useOrientation.native.ts
+++ src/utils/hooks/useOrientation.native.ts
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 
-import Orientation, { OrientationType, useOrientationChange } from 'react-native-orientation-locker';
+import Orientation, { OrientationType, useDeviceOrientationChange } from 'react-native-orientation-locker';
 
 const useOrientation = () => {
   const [currentOrientation, setOrientation] = useState<OrientationType>(Orientation.getInitialOrientation());
-  useOrientationChange(setOrientation);
+  useDeviceOrientationChange(setOrientation);
 
   return currentOrientation;
 };
