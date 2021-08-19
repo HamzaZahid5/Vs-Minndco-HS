000993b0160b8b322cac9544868fa2c1f5457c81
Joaco <joakoitria98@gmail.com>
2021-08-19 10:40:48 -0300

    LayoutAnimation.configureNext and setTimeout removed using fadeout callback instead

--- src/components/FadeEffect/index.js
+++ src/components/FadeEffect/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /**
  * FADES IN OR OUT ITS CHILDREN.
  * USED INTO LIFESAVER CHAT BOT, BASIC TUTORIAL AND LOGIN.
@@ -9,23 +10,17 @@ import { Animated, LayoutAnimation } from 'react-native';
 const FadeEffect = ({ show, children, duration = 400, style }) => {
   const [animation] = useState(new Animated.Value(0));
   const [appear, setAppear] = useState(false);
-  const appearTimeIDRef = useRef(0);
   useEffect(() => {
     if (show) {
-      LayoutAnimation.configureNext({ ...LayoutAnimation.Presets.easeInEaseOut, duration: duration / 2 }, () => {
-        fadeIn();
-      });
-      setAppear(true);
+      fadeIn();
     } else {
       fadeOut();
-      appearTimeIDRef.current = setTimeout(() => {
-        setAppear(false);
-      }, duration);
     }
-    return () => clearTimeout(appearTimeIDRef.current);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [show]);
   const fadeIn = () => {
+    if (animation._value === 1) return;
+    setAppear(true);
     Animated.timing(animation, {
       toValue: 1,
       timing: duration,
@@ -33,11 +28,14 @@ const FadeEffect = ({ show, children, duration = 400, style }) => {
     }).start();
   };
   const fadeOut = () => {
+    if (animation._value === 0) return;
     Animated.timing(animation, {
       toValue: 0,
       duration: duration,
       useNativeDriver: true,
-    }).start();
+    }).start(() => {
+      if (animation._value === 0) setAppear(false);
+    });
   };
   const animatedStyle = [
     {
