4042608fc8ff3da78b305fdbd4f7455c227067d1
Joaco <joakoitria98@gmail.com>
2021-08-26 18:55:55 -0300

    Added loading icon to player when is loading and buffering. Using setState callback to set state. Check if sound exists when sliding is complete Imporve sliding complete antion

--- src/components/AudioPlayer/index.js
+++ src/components/AudioPlayer/index.js
@@ -1,22 +1,24 @@
+/* eslint-disable no-shadow */
 /**
  * AUDIO PLAYER USED INTO PROGRAM ACTIVITY SCREEN. ONE OF MANY OPTIONS LIKE VR, 2D VIDEO AND QUESTIONS
  */
 import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
-import { View, StyleSheet, Platform } from 'react-native';
+import { View, StyleSheet, Platform, Animated } from 'react-native';
 import { IconButton, useTheme } from 'react-native-paper';
 import { Audio } from 'expo-av';
 import Slider from '@react-native-community/slider';
 import Color from 'color';
 
+const AnimatedIconButton = Animated.createAnimatedComponent(IconButton);
+
 let tId;
 const ActivityPlayerVideo = ({ audioURI = '', didJustFinish = null }) => {
   const statusTId = useRef();
   const isSliding = useRef();
-  // const isPlaying = useRef();
   const shouldPlay = useRef();
   const [isPlaying, setIsPlaying] = useState();
-  // const [shouldPlay, setShouldPlay] = useState();
+  const [isLoaded, setIsLoaded] = useState();
   const [sound, setSound] = useState();
   const [duration, setDuration] = useState(1);
   const [currentTime, setCurrentTime] = useState(0);
@@ -39,18 +41,17 @@ const ActivityPlayerVideo = ({ audioURI = '', didJustFinish = null }) => {
     // shouldPlay: false
     // uri: "https://firebasestorage.googleapis.com/v0/b/mindcotine-v4-production.appspot.com/o/lifesaver%2FAudio_VAS_1_EN.mp3?alt=media&token=59841ed4-446e-4b0f-b168-e0a1f3f1f938"
     // volume: 1
-    if (currentTime !== status.positionMillis) {
-      setCurrentTime(status.positionMillis);
-    }
+
     if (duration !== status.durationMillis && !isNaN(status.durationMillis)) {
       setDuration(status.durationMillis);
     }
-    if (currentBuffering !== status.isBuffering) {
-      setBuffering(status.isBuffering);
-    }
-    if (isPlaying !== status.isPlaying) {
-      setIsPlaying(status.isPlaying);
-    }
+    setCurrentTime(currentTime => (currentTime !== status.positionMillis ? status.positionMillis : currentTime));
+
+    setBuffering(currentBuffering => (currentBuffering !== status.isBuffering ? status.isBuffering : currentBuffering));
+
+    setIsPlaying(isPlaying => (isPlaying !== status.isPlaying ? status.isPlaying : isPlaying));
+
+    setIsLoaded(isLoaded => (isLoaded !== status.isLoaded ? status.isLoaded : isLoaded));
   };
   async function loadSound(uri) {
     const downloadFirst = true;
@@ -101,18 +102,32 @@ const ActivityPlayerVideo = ({ audioURI = '', didJustFinish = null }) => {
       clearTimeout(statusTId.current);
     };
   }, []);
+
+  useEffect(() => {
+    if (isLoaded === true && currentBuffering === false && shouldPlay.current === true && isPlaying === false)
+      playSound();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentBuffering, isLoaded]);
+
   const seekTo = val => {
+    if (!sound) return;
     setCurrentTime(val);
     sound.setPositionAsync(val);
   };
   const slidingStart = () => {
     if (isPlaying) {
-      stopSound();
+      pauseSound();
     }
     isSliding.current = true;
   };
   const slidingComplete = val => {
-    setCurrentTime(val);
+    if (!sound) return;
+    if (val === duration) {
+      shouldPlay.current = false;
+      stopSound();
+      didJustFinish();
+      return;
+    }
     seekTo(val);
     if (shouldPlay.current && !isPlaying) {
       playSound();
@@ -130,11 +145,12 @@ const ActivityPlayerVideo = ({ audioURI = '', didJustFinish = null }) => {
     <View style={styles.playerContainer}>
       <View style={styles.controls}>
         <IconButton
-          icon={isPlaying ? 'pause' : 'play'}
+          icon={isLoaded === true && currentBuffering === false ? (isPlaying ? 'pause' : 'play') : 'loading'}
           size={30}
           color="white"
-          style={styles.playIcon}
+          style={{ ...styles.playIcon }}
           onPress={() => {
+            if (isLoaded === false || currentBuffering === true) return;
             shouldPlay.current = !shouldPlay.current;
             if (shouldPlay.current && !isPlaying) {
               playSound();
@@ -178,6 +194,8 @@ const getStyles = theme =>
     controls: {
       flexDirection: 'row',
       marginBottom: 25,
+      justifyContent: 'center',
+      alignItems: 'center',
       height: 80,
     },
     progressIconsContainer: {
@@ -190,3 +208,12 @@ const getStyles = theme =>
       // backgroundColor: 'lime',
     },
   });
+const sigmoidalEasingGenerator = (p, s) => {
+  const c = 2 / (1 - s) - 1;
+  const f = (t, n) => Math.pow(t, c) / Math.pow(n, c - 1);
+
+  return t => {
+    if (t < p) return f(t, p);
+    else return 1 - f(1 - t, 1 - p);
+  };
+};
