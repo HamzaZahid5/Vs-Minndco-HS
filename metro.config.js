9be30ae03aa18cbe8ef8b7749418083962a5b090
Cosmitar <mhherrera31@gmail.com>
2021-08-27 14:52:47 -0300

    support to import mp3 (and others)

--- metro.config.js
+++ metro.config.js
@@ -2,12 +2,13 @@ const { getDefaultConfig } = require('metro-config');
 
 module.exports = (async () => {
   const {
-    resolver: { sourceExts },
+    resolver: { assetExts, sourceExts },
   } = await getDefaultConfig();
 
   return {
     resolver: {
       sourceExts: [...sourceExts, 'js', 'json', 'ts', 'tsx'],
+      assetExts: [...assetExts, 'mp3', 'png', 'jpg'],
     },
     transformer: {
       assetPlugins: ['expo-asset/tools/hashAssetFiles'],
