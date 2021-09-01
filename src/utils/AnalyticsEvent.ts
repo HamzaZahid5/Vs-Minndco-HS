35d4c982834762c4b9cbb5ff8bf0b7333f023dff
Joaco <joakoitria98@gmail.com>
2021-09-01 16:16:38 -0300

    Exporting default analyticEvent function

--- src/utils/AnalyticsEvent.ts
+++ src/utils/AnalyticsEvent.ts
@@ -1,9 +1,11 @@
 import analytics from '@react-native-firebase/analytics';
 import { AnalyticEventType } from '../../types';
 
-export const AnalyticEvent = async <T extends keyof AnalyticEventType>(
+const AnalyticEvent = async <T extends keyof AnalyticEventType>(
   event: T,
   eventArgument: AnalyticEventType[T],
 ): Promise<void> => {
   return await analytics().logEvent(event, eventArgument);
 };
+
+export default AnalyticEvent;
