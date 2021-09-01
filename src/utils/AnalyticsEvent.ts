be00c1629d9131b3a76493624d972030240954b5
Joaco <joakoitria98@gmail.com>
2021-09-01 16:19:23 -0300

    eventArgument as optional parameter

--- src/utils/AnalyticsEvent.ts
+++ src/utils/AnalyticsEvent.ts
@@ -3,7 +3,7 @@ import { AnalyticEventType } from '../../types';
 
 const AnalyticEvent = async <T extends keyof AnalyticEventType>(
   event: T,
-  eventArgument: AnalyticEventType[T],
+  eventArgument?: AnalyticEventType[T],
 ): Promise<void> => {
   return await analytics().logEvent(event, eventArgument);
 };
