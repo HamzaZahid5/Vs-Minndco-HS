c44186c8f880911b4dcaf2e4d318bc5d493039b3
Joaco <joakoitria98@gmail.com>
2022-02-24 16:35:36 -0300

    Added drawer

---
+++ src/screens/DrawerNavigator/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { createDrawerNavigator, DrawerContentComponentProps, DrawerNavigationProp } from '@react-navigation/drawer'
+import CustomDrawerContent from './CustomDrawerContent'
+import TabsHomeScreen from '../TabsNavigator'
+
+export type DrawerParamList = {
+  DrawerHome: undefined
+}
+
+export type HomeScreenDrawerNavigationProp = DrawerNavigationProp<DrawerParamList, 'DrawerHome'>
+
+const Drawer = createDrawerNavigator<DrawerParamList>()
+
+const getDrawerContent = (props: DrawerContentComponentProps) => <CustomDrawerContent {...props} />
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator
+      // openByDefault
+      initialRouteName="DrawerHome"
+      drawerContent={getDrawerContent}
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: '#fcfcfc',
+        },
+        lazy: false,
+      }}
+    >
+      <Drawer.Screen name="DrawerHome" component={TabsHomeScreen} options={{ headerShown: false }} />
+    </Drawer.Navigator>
+  )
+}
+
+export default DrawerNavigator
+
+/*
+container: {
+    flex: 1,
+  },
+  gradient: {
+    flex: 1,
+    position: 'relative',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  */
