0b29dbb64c222f01dd88df2bad2916a3420927bc
Joaco <joakoitria98@gmail.com>
2022-03-04 20:37:32 -0300

    Added animated parallax hook

---
+++ src/utils/hooks/useAnimatedParallax.tsx
@@ -0,0 +1,34 @@
+import React, { useRef } from 'react'
+import { Animated, StyleProp, ViewStyle } from 'react-native'
+
+type params = { styles?: StyleProp<ViewStyle>[]; marginOffsett?: number; interpolationRange?: number }
+
+const useAnimatedParallax = ({ styles = [], marginOffsett = -20, interpolationRange = 155 }: params) => {
+  const scrollViewAnimatedOffset = useRef(new Animated.Value(0))
+  const fadeAnimInterpolation = scrollViewAnimatedOffset.current.interpolate({
+    inputRange: [0, interpolationRange],
+    outputRange: [1, 0],
+    extrapolate: 'clamp',
+  })
+  const marginAnimInterpolation = scrollViewAnimatedOffset.current.interpolate({
+    inputRange: [0, interpolationRange],
+    outputRange: [0, marginOffsett],
+    extrapolate: 'clamp',
+  })
+
+  const AnimatedViewElement = ({ children }: { children?: React.ReactNode }) => (
+    <Animated.View
+      style={[...styles, { opacity: fadeAnimInterpolation, transform: [{ translateY: marginAnimInterpolation }] }]}
+    >
+      {children}
+    </Animated.View>
+  )
+  const animatedEvent = () =>
+    Animated.event([{ nativeEvent: { contentOffset: { y: scrollViewAnimatedOffset.current } } }], {
+      useNativeDriver: false,
+    })
+
+  return { AnimatedViewElement, animatedEvent, animationControl: scrollViewAnimatedOffset }
+}
+
+export default useAnimatedParallax
