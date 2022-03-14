21908be4e35910a3adac519c121be282e6a86627
Joaco <joakoitria98@gmail.com>
2022-03-14 16:53:39 -0300

    Using completeActivity callable

--- src/utils/hooks/useActivityActions.ts
+++ src/utils/hooks/useActivityActions.ts
@@ -8,8 +8,8 @@ import {
   updateActivityCounter,
 } from '../../services/Firestore'
 import functions from '../../services/Functions'
-import { LAST_ACTIVITY_AT, ACTIVITY_DAYS_IN_A_ROW } from '../../store/selectors'
-import { getModuleNumberFromKey, getLevelNumberFromKey } from '../helpers'
+import { LAST_ACTIVITY_AT, ACTIVITY_DAYS_IN_A_ROW, USER_PROFILE } from '../../store/selectors'
+import { getModuleNumberFromKey, getLevelNumberFromKey, formatAsset } from '../helpers'
 import useNextActivity from './useNextActivity'
 
 const getTypeByCategory = (category?: string) => {
@@ -29,8 +29,8 @@ const useActivityActions = () => {
   // const dispatch = useDispatch();
   const { nextActivity } = useNextActivity()
   const lastActivityDate = useSelector(LAST_ACTIVITY_AT)
-  const streakCount = useSelector(ACTIVITY_DAYS_IN_A_ROW)
   const dispatch = useDispatch()
+  const { language, gender } = useSelector(USER_PROFILE)
 
   const lastActivityAt = moment(lastActivityDate).format('YYYY-MM-DD')
   const lastActivityNotToday = lastActivityAt !== moment().format('YYYY-MM-DD')
@@ -39,30 +39,22 @@ const useActivityActions = () => {
   const streakLost = () => lastActivityNotToday && lastActivityNotYesterday
   // only matter if is not another activity in the same day, all other cases increment the streak.
   // we don't care about streak lost since that happens when login
-  const isConsecutiveDay = () => lastActivityNotToday
 
   return {
     saveActivityDone: async (activityKey: string, answer = '') => {
       //Dispatch here, so when the PathEnding screen is loaded, useTodaysActivityDone have the correct date.
       dispatch({ type: 'user/setLastActivityAt', payload: { date: getFirestoreTimestamp(), key: activityKey } })
 
-      const treatment_module = getModuleNumberFromKey(activityKey)
-      const treatment_level = getLevelNumberFromKey(activityKey)
-      const from = 'program'
-
-      await saveActivityDoneIntoDB({
-        treatment_module,
-        treatment_level,
-        activityKey,
-        streak: isConsecutiveDay() ? streakCount + 1 : streakCount,
-      })
-
-      await functions().httpsCallable('logActivityDone')({
-        activity: nextActivity,
-        activityKey,
-        answer,
-        from,
-      })
+      if (nextActivity) {
+        await functions().httpsCallable('completeActivity')({
+          activity: {
+            ...nextActivity,
+            asset: formatAsset(nextActivity.asset, language, gender),
+            key: activityKey,
+          },
+          answer,
+        })
+      }
 
       // const category = getTypeByCategory(nextActivity?.category)
       // if (category) {
