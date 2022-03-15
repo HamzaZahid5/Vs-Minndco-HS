342afcfaf941228a54a92a7b12e0666c1c6f8908
Joaco <joakoitria98@gmail.com>
2022-03-15 11:00:27 -0300

    Updated tutorial flag

--- src/services/Firestore/index.ts
+++ src/services/Firestore/index.ts
@@ -89,7 +89,7 @@ export const updateUserHardware = ({
 
 export const updateBasicTutorialCompleted = () =>
   updateProfile({
-    'flags.show_basics_tutorial': false,
+    flag_show_basics_tutorial: false,
   })
 
 export const getKitById = (code: string) => firestore().collection('kits').doc(code).get()
