69e2a4f4028b87fa66d341bbb050ae27169db713
Jorge Alberto Santamaria <57959083+jorgesantamaria96@users.noreply.github.com>
2023-02-07 22:21:28 -0300

    Fix/android env (#37)
    * fix e
    
    * quitday in profile
    
    * update smoke recorder popup
    
    * fix save quit_day
    
    * save progress on tutorial
    
    * resolve error in basics tutorials and crash in redeem code
    
    * fix screens with keyboard, fin in onboarding and flags of tutorial
    
    * fix icons smalls in SmokeRecordScreen
    
    * add logo in login and text center in ForgotPassword
    
    * update video kit presentation
    
    * update spent program
    
    * fix error login code in forgot password
    
    * some basics changes
    
    * finished upload to internal testing android
    
    * update vr
    
    * resolve errors of compiling
    
    * update vr

--- env/index.ts
+++ env/index.ts
@@ -1,6 +1,7 @@
 // @ts-ignore: non-ts file
 import active, { IP } from './active.env'
 import packageJson from '../package.json'
+import { Platform } from 'react-native'
 
 type EnvConfig = {
   name: string
@@ -20,8 +21,7 @@ const envs: Record<EnvNames, EnvConfig> = {
     name: 'production',
     SmartlookApiKey: 'ef7b65fc05cee7e87d7014619355120f7a8a47e6',
     APP_VERSION: packageJson.version,
-    webVrURL: 'mindcotine-player.web.app',
-    // webVrURL: 'mindco-vr-player-v5.web.app',
+    webVrURL: Platform.OS === "android" ? 'mindco-vr-player-v5.web.app' : 'mindcotine-player.web.app',
     surveys: {
       stressSurvey: 'https://survey.zohopublic.com/zs/zaCzbK',
       resilienceSurvey: 'https://survey.zohopublic.com/zs/8PCzC8',
