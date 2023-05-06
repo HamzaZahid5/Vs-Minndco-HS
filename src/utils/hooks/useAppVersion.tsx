8ca5332e4f55e7f4f90972da72b1bd0dffdf053b
Jorge Santamaria <jorgealberto.santamaria@nttdata.com>
2023-05-06 12:44:25 -0300

    some changes

--- src/utils/hooks/useAppVersion.tsx
+++ src/utils/hooks/useAppVersion.tsx
@@ -1,71 +1,10 @@
 import React, { useEffect } from 'react'
 import axios from 'axios'
-import { Linking, Platform } from 'react-native'
-import { Button, Paragraph, Row, Subheading } from '@mindcoxr/rob'
+import { Platform } from 'react-native'
 import { useDispatch } from 'react-redux'
 import pkg from '../../../package.json'
 import { RootStackParamList } from '../../../types'
-import { translate } from '../localization'
 import { StackNavigationProp } from '@react-navigation/stack'
-
-// const openStore = () => {
-//   if (Platform.OS === 'ios') {
-//     const link = 'itms-apps://apps.apple.com/us/app/mindcotine/id1506021271'
-//     Linking.canOpenURL(link)
-//       .then(supported => {
-//         supported && Linking.openURL(link)
-//       })
-//       .catch(err => {
-//         console.log(err)
-//       })
-//   } else {
-//     const linkPlayStore = 'https://play.google.com/store/apps/details?id=com.habitfly.mindcotine&hl=es_419'
-//     Linking.canOpenURL(linkPlayStore)
-//       .then(supported => {
-//         supported && Linking.openURL(linkPlayStore)
-//       })
-//       .catch(err => {
-//         console.log(err)
-//       })
-//   }
-// }
-
-// const PopupContent = ({ close }: { close: () => Promise<void> }) => {
-//   return (
-//     <>
-//       <Row gutter={2}>
-//         <Subheading>{translate('screens.UpdateScreen.title')}</Subheading>
-//       </Row>
-//       <Row grow justifyContentOnGrow="flex-start" gutter={10}>
-//         <Paragraph size="medium" weight="normal" textAlign="left">
-//           {translate('screens.UpdateScreen.description')}
-//         </Paragraph>
-//       </Row>
-//       <Row gutter={10} grow justifyContentOnGrow="flex-end">
-//         <Button
-//           role="primary"
-//           compact
-//           onPress={async () => {
-//             await close()
-//             openStore()
-//           }}
-//         >
-//           {translate('screens.UpdateScreen.confirm')}
-//         </Button>
-//         <Button
-//           role="secondary"
-//           compact
-//           outline
-//           onPress={async () => {
-//             await close()
-//           }}
-//         >
-//           {translate('screens.UpdateScreen.later')}
-//         </Button>
-//       </Row>
-//     </>
-//   )
-// }
 
 const versionsInStore = async (
   appId: string,
