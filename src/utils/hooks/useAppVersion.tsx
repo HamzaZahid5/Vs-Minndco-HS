1ba0db757b5af3c52b9d6e0fb9175b88085d4a81
Jorge Santamaria <jorge.santamaria966@gmail.com>
2023-06-01 19:48:37 -0300

    update appVersion in app

--- src/utils/hooks/useAppVersion.tsx
+++ src/utils/hooks/useAppVersion.tsx
@@ -5,8 +5,9 @@ import { useDispatch } from 'react-redux'
 import pkg from '../../../package.json'
 import { RootStackParamList } from '../../../types'
 import { StackNavigationProp } from '@react-navigation/stack'
+import { useFirestoreListener } from '../../services/Firestore'
 
-const versionsInStore = async (
+const versionsInStoreAppleGoogle = async (
   appId: string,
   packageName: string,
   dispatch: ReturnType<typeof useDispatch>,
@@ -47,11 +48,51 @@ const versionsInStore = async (
   }
 }
 
+interface VersionProps {
+  android: string
+  ios: string
+}
+
+const versionInStorageServer = (
+  appVersion: Record<string, unknown> | null | undefined,
+  dispatch: ReturnType<typeof useDispatch>,
+  navigation: StackNavigationProp<RootStackParamList>,
+) => {
+  let firestoreVersion = appVersion as unknown as VersionProps
+  if (firestoreVersion && Platform.OS === 'ios') {
+    if (firestoreVersion.ios !== pkg.version) {
+      dispatch({ type: 'user/setShowNeedUpdate', payload: true })
+      setTimeout(() => {
+        navigation && navigation.navigate('UpdateApp')
+      }, 100)
+    } else {
+      dispatch({ type: 'user/setShowNeedUpdate', payload: false })
+      setTimeout(() => {
+        navigation && navigation.navigate('Home')
+      }, 100)
+    }
+  } else {
+    if (firestoreVersion && firestoreVersion.android !== pkg.version) {
+      dispatch({ type: 'user/setShowNeedUpdate', payload: true })
+      setTimeout(() => {
+        navigation && navigation.navigate('UpdateApp')
+      }, 100)
+    } else {
+      dispatch({ type: 'user/setShowNeedUpdate', payload: false })
+      setTimeout(() => {
+        navigation && navigation.navigate('Home')
+      }, 100)
+    }
+  }
+}
+
 const useAppVersion = (navigation: StackNavigationProp<RootStackParamList>) => {
   const dispatch = useDispatch()
+  const appVersion = useFirestoreListener('app_version', 'ileSVeW0Qba7ke0xDsDQ')
   useEffect(() => {
-    versionsInStore('1506021271', 'com.habitfly.mindcotine', dispatch, navigation)
-  }, [])
+    // versionsInStoreAppleGoogle('1506021271', 'com.habitfly.mindcotine', dispatch, navigation)
+    versionInStorageServer(appVersion, dispatch, navigation)
+  }, [appVersion])
 }
 
 export default useAppVersion
