cf04c9ec9462ef8b5fa472be36c9034780692a03
Jorge Santamaria <jorgealberto.santamaria@nttdata.com>
2023-09-21 17:49:36 -0300

    update compile sdk 31

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -146,7 +146,7 @@ const HomeScreen = ({ navigation }: { navigation: HomeScreenNavigationProp }) =>
   const isLastActivityDone = isLastActivity && nextActivityKey && isActivityDone(nextActivityKey, progress)
   // const isPremium = useSelector(IS_PREMIUM)
 
-  // useAppVersion(navigation as StackNavigationProp<RootStackParamList>)
+  useAppVersion(navigation as StackNavigationProp<RootStackParamList>)
   console.log(progress.length)
   // useEffect(() => {
   //   if (showBasicTutorial && !hasSmokeRecords) {
