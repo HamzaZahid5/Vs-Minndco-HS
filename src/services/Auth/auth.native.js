0c91582ff5981b72e0a2a403cd8e5231d8bef9b9
Matias Mrz <144380209+MzzuMrz@users.noreply.github.com>
2023-10-11 18:55:28 -0300

    fix: Android prod (#42)

--- src/services/Auth/auth.native.js
+++ src/services/Auth/auth.native.js
@@ -1,7 +1,20 @@
+/* eslint-disable no-console */
 import config from '../../../env'
 import auth from '@react-native-firebase/auth'
-if (config.name === 'test') {
-  auth().useEmulator(`http://${config.emulatorIp}:9099`)
+
+// Verifica si config existe antes de intentar acceder a sus propiedades
+const configName = config?.name ?? 'defaultName'
+const emulatorIp = config?.emulatorIp ?? 'defaultIp'
+console.log(configName, emulatorIp)
+
+try {
+  // Solo intenta usar el emulador si el nombre de la configuración es 'test'
+  if (configName === 'test') {
+    auth().useEmulator(`http://${emulatorIp}:9099`)
+  }
+} catch (error) {
+  // Maneja cualquier error que pueda ocurrir cuando se intenta usar el emulador
+  console.error('Failed to use emulator:', error.message)
 }
 
 export default () => auth()
